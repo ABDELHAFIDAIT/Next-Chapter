f8d3483f9ddb4139119a86055e224a466e1b6092
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-04-13 00:34:31 +0100

    creating the footer section in index page and add script for faq accordion

---
+++ frontend/js/app.js
@@ -0,0 +1,10 @@
+// FAQ 
+const faqArrow = document.querySelectorAll('.faq-arrow');
+const faqAnswer = document.querySelectorAll('.faq-answer');
+
+faqArrow.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
+        faqAnswer[index].classList.toggle('hidden');
+        arrow.classList.toggle('rotate-180');
+    });
+});
