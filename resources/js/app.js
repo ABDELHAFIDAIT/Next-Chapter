e1136e2c806f2ba8d42a52e2f9f0e981acf36390
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-04-14 11:48:21 +0100

    Add Homepage using blade

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+// FAQ 
+const faqArrow = document.querySelectorAll('.faq-arrow');
+const faqAnswer = document.querySelectorAll('.faq-answer');
+
+faqArrow.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
+        faqAnswer[index].classList.toggle('hidden');
+        arrow.classList.toggle('rotate-180');
+    });
+});
